c9706d08ff63769a4b7b3cddc3997b195f30cfbe
이원현 <leewonhyun@iwonhyeon-ui-MacBookPro.local>
2025-08-28 14:21:34 +0900

    fix: fallback edit version 16

--- src/app/(with-header)/layout.tsx
+++ src/app/(with-header)/layout.tsx
@@ -7,7 +7,24 @@ export const viewport = {
   maximumScale: 1,
   userScalable: false,
 };
-
+export const metadata = {
+  metadataBase: new URL("https://2025eskara-client-table1.vercel.app/"),
+  title: "2025 eskara 배트와 매트 1번 테이블",
+  description: "배트와 매트 1번 테이블 주문 페이지입니다",
+  openGraph: {
+    title: "배트와 매트",
+    description: "배트와 매트 1번 테이블 주문 페이지입니다",
+    siteName: "배트와 매트",
+    images: [
+      {
+        url: "/title.jpeg", // 썸네일 이미지 URL
+        alt: "썸네일 이미지 설명",
+      },
+    ],
+    locale: "ko_KR",
+    type: "website",
+  },
+};
 const SF_pro = localFont({
   src: "../../../font/SF-Pro.ttf",
   weight: "400",
