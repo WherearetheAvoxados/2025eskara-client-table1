a2d91f55817b166d0ca39c8e6a8378c0dc5f88a9
이원현 <leewonhyun@iwonhyeon-ui-MacBookPro.local>
2025-08-28 12:06:57 +0900

    fix: fallback edit version 3

--- src/app/(with-header)/@mordal/order/page.tsx
+++ src/app/(with-header)/@mordal/order/page.tsx
@@ -2,7 +2,7 @@ import BillModal from "../../../../../components/bill";
 import localFont from "next/font/local";
 //table id string으로 변수로 받아오기
 const BM_HANNA = localFont({
-  src: "../font/BMHANNAAir_ttf.ttf",
+  src: "../../../../../font/BMHANNAAir_ttf.ttf",
   weight: "400", // ttf라면 weight는 그냥 "400"으로 지정
   style: "normal",
 });
--- src/app/(with-header)/@mordal/temporder/page.tsx
+++ src/app/(with-header)/@mordal/temporder/page.tsx
@@ -5,7 +5,7 @@ import ModalSkeleton from "../../../../../components/modalskeleton";
 import localFont from "next/font/local";
 
 const BM_HANNA = localFont({
-  src: "../font/BMHANNAAir_ttf.ttf",
+  src: "../../../../../font/BMHANNAAir_ttf.ttf",
   weight: "400", // ttf라면 weight는 그냥 "400"으로 지정
   style: "normal",
 });
